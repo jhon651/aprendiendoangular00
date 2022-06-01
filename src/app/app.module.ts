404087750a0529e2b8a1fd31be471b00f65d1c64
Freddy Valencia <ing.desa12@syspotec.com>
2022-06-01 09:58:01 -0500

    Se inicia con la creación del primer formulario, se el componente del formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Formulario1Component } from './formulario1/formulario1.component';
+//Importar libreria para las rutas
+import { Routes, RouterModule } from '@angular/router';
+
+const routes:Routes = [
+  {path:'', component: Formulario1Component},
+  {path: 'formulario1', component: Formulario1Component},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Formulario1Component,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
